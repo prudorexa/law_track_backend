a4b3b8bb01e094a17d97efd8b93415227a64cd9f
prudorexa <162288690+prudorexa@users.noreply.github.com>
2024-07-30 10:35:07 +0300

    add changes

--- service/models.py
+++ service/models.py
@@ -31,7 +31,6 @@ class Service(models.Model):
     
 
 class Lawyer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
 
